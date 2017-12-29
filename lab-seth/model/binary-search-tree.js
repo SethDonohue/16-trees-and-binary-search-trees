a770dc0182e73730a74263b0e036bab3c87dfd9a
SethDonohue <donohue.seth@gmail.com>
2017-12-29 08:35:56 -0800

    BST refactored for keys...

--- lab-seth/model/binary-search-tree.js
+++ lab-seth/model/binary-search-tree.js
@@ -1,42 +1,43 @@
 'use strict';
 
 class BinarySearchTree{
-  constructor(value){
+  constructor(key, value){
+    this.key = key;
     this.value = value;
     this.left = null;
     this.right = null;
   }
 
-  insert(value){
-    if(typeof value !== 'number') throw new TypeError('Binary Search Tree - value should be a number');
+  insert(key, value){
+    if(typeof key !== 'string') throw new TypeError('Binary Search Tree - <Key> miust be a string');
 
-    if(this.value === value) throw new TypeError('Binary Search Tree - value is already present');
+    if(this.key === key) throw new TypeError('Binary Search Tree - key is already present');
 
-    if(value < this.value){
+    if(key < this.key){
       if(!this.left){
-        this.left = new BinarySearchTree(value);
+        this.left = new BinarySearchTree(key, value);
         return;
       }
-      this.left.insert(value);
+      this.left.insert(key, value);
       return;
     }
     if(!this.right){
-      this.right = new BinarySearchTree(value);
+      this.right = new BinarySearchTree(key, value);
       return;
     }
-    this.right.insert(value);
+    this.right.insert(key, value);
     return;
   }
 
-  find(value){
-    if(value === this.value) return this;
+  find(key){
+    if(key === this.key) return this;
     
-    if(value < this.value){
-      if(this.left !== null) return this.left.find(value); return false;
+    if(key < this.key){
+      if(this.left !== null) return this.left.find(key); return false;
     }
     
-    if(value > this.value){
-      if(this.right !== null) return this.right.find(value); return false;
+    if(key > this.key){
+      if(this.right !== null) return this.right.find(key); return false;
     }
    
   }
@@ -49,45 +50,45 @@ class BinarySearchTree{
     if(!this.right) return this; return this.right.getMax();
   }
 
-  getParent(value){
-    if(this.value === value) return null; // value equals the first node which means there are no parents then return null.
+  getParent(key){
+    if(this.key === key) return null; // key equals the first node which means there are no parents then return null.
 
-    if(!this.left && !this.right) return -1;// if no children and only one node exists and it does not equal the value we are searching for return -1;
+    if(!this.left && !this.right) return -1;// if no children and only one node exists and it does not equal the key we are searching for return -1;
 
-    if(!this.right && this.left.value === value) return this;
-    if(!this.left && this.right.value === value) return this;
+    if(!this.right && this.left.key === key) return this;
+    if(!this.left && this.right.key === key) return this;
 
     if(this.right && this.left)
-      if(this.right.value === value || this.left.value === value) return this; //no children left so this is the parent
+      if(this.right.key === key || this.left.key === key) return this; //no children left so this is the parent
 
-    if(value < this.value) return this.left.getParent(value); return this.right.getParent(value); //go left first, else go right.
+    if(key < this.key) return this.left.getParent(key); return this.right.getParent(key); //go left first, else go right.
 
   }
 
-  remove(value){
-    let parent = this.getParent(value);
+  remove(key){
+    let parent = this.getParent(key);
     let current = this;
 
-    if(parent === null) return null; // no parents exist but the value we are looking for is in this single node as determined by getParent, so return null.
+    if(parent === null) return null; // no parents exist but the key we are looking for is in this single node as determined by getParent, so return null.
 
-    if(parent === -1) return this; // no parents exist and the value we are loking for is not in this tree as determined by getparent, so return original tree.
+    if(parent === -1) return this; // no parents exist and the key we are loking for is not in this tree as determined by getparent, so return original tree.
 
     let removeHelper = function (current){
-      if(current.value === value){
+      if(current.key === key){
       
         if(current.left === null && current.right === null) return null; //node is only node to remove
 
-        if(current.value > parent.value ){
+        if(current.key > parent.key ){
           (current.right.getMin()).left = current.left;
           parent.right = current.right;
         }
-        if(current.value < parent.value ){
+        if(current.key < parent.key ){
           (current.right.getMin()).left = current.left;
           parent.left = current.right;
         }
       }
-      if(value < current.value) return removeHelper(current.left);
-      if(value > current.value) return removeHelper(current.right);
+      if(key < current.key) return removeHelper(current.left);
+      if(key > current.key) return removeHelper(current.right);
     };
     removeHelper(this);
     return this;
